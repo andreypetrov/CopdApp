ff4711c726cf1c88e55a4f84f8c036616bf54b4a
Andrey Petrov <andrey.petrov@td.com>
2015-05-11 15:50:17 -0400

    Testing the voice recording

---
+++ app/src/main/java/com/petrodevelopment/copdapp/record/AudioRecord.java
@@ -0,0 +1,108 @@
+package com.petrodevelopment.copdapp.record;
+
+/**
+ * Created by user on 15-05-11.
+ */
+/*
+ * The application needs to have the permission to write to external storage
+ * if the output file is written to the external storage, and also the
+ * permission to record audio. These permissions must be set in the
+ * application's AndroidManifest.xml file, with something like:
+ *
+ * <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" />
+ * <uses-permission android:name="android.permission.RECORD_AUDIO" />
+ *
+ */
+
+import android.app.Activity;
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import java.io.IOException;
+
+
+public class AudioRecord extends Activity {
+    private static final String LOG_TAG = "AudioRecord";
+
+    private String mFileName = null;
+    private MediaRecorder mRecorder = null;
+    private MediaPlayer mPlayer = null;
+
+
+
+
+    public AudioRecord(String fileName) {
+        mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
+        mFileName += "/";
+        mFileName += "audiorecordtest.3gp";
+    }
+
+
+    public void startPlaying() {
+        mPlayer = new MediaPlayer();
+        try {
+            mPlayer.setDataSource(mFileName);
+            mPlayer.prepare();
+            mPlayer.start();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+    }
+
+    public void pausePlaying() {
+        mPlayer.pause();
+    }
+
+    public void unpausePlaying() {
+        mPlayer.start();
+    }
+
+    public void stopPlaying() {
+        mPlayer.release();
+        mPlayer = null;
+    }
+
+    private void startRecording() {
+        mRecorder = new MediaRecorder();
+        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mRecorder.setOutputFile(mFileName);
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            mRecorder.prepare();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+
+        mRecorder.start();
+    }
+
+
+    private void stopRecording() {
+        mRecorder.stop();
+        mRecorder.release();
+        mRecorder = null;
+    }
+
+
+    public void releaseRecorderAndPlayer() {
+        if (mRecorder != null) {
+            mRecorder.release();
+            mRecorder = null;
+        }
+
+        if (mPlayer != null) {
+            mPlayer.release();
+            mPlayer = null;
+        }
+    }
+}
