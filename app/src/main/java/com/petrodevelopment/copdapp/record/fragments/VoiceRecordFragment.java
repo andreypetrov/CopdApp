8e6598f7d5afb0815be90d44a5640969d2dffc53
Andrey Petrov <andrey.petrov@td.com>
2015-05-19 11:44:26 -0400

    Created new voice fragment. In progress

--- app/src/main/java/com/petrodevelopment/copdapp/record/fragments/VoiceRecordFragment.java
+++ app/src/main/java/com/petrodevelopment/copdapp/record/fragments/VoiceRecordFragment.java
@@ -1,18 +1,38 @@
 package com.petrodevelopment.copdapp.record.fragments;
 
+import android.app.Activity;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.petrodevelopment.copdapp.R;
 import com.petrodevelopment.copdapp.fragments.AppointmentsFragment;
 import com.petrodevelopment.copdapp.fragments.SectionFragment;
 
+import java.io.IOException;
+
 /**
  * Created by user on 15-05-14.
  */
 public class VoiceRecordFragment extends SectionFragment {
+    private static final String LOG_TAG = "AudioRecordTest";
+    private static String mFileName;
+    private Button mRecordButton;
+    private Button mPlayButton;
+
+    private boolean isRecording = false;
+    private boolean isPlaying = false;
+
+    private MediaRecorder mRecorder;
+    private MediaPlayer mPlayer;
+
+
     /**
      * Returns a new instance of this fragment for the given section
      * number.
@@ -28,6 +48,104 @@ public class VoiceRecordFragment extends SectionFragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_record_voice, container, false);
 
+        initView(rootView);
+        mFileName = Environment.getExternalStorageDirectory().getAbsolutePath() + "/audiorecordtest.3gp";
+
+
         return rootView;
     }
+
+    private void initView(View rootView) {
+        initRecordButton(rootView);
+        initPlayButton(rootView);
+
+
+
+
+    }
+
+    private void initRecordButton(View rootView) {
+        mRecordButton = (Button) rootView.findViewById(R.id.record_btn);
+        mRecordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isPlaying) {
+
+                }
+            }
+        });
+    }
+
+
+    private void initPlayButton(View rootView) {
+        mPlayButton = (Button) rootView.findViewById(R.id.play_btn);
+
+    }
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        super.onPause();
+        if (mRecorder != null) {
+            mRecorder.release();
+            mRecorder = null;
+        }
+
+        if (mPlayer != null) {
+            mPlayer.release();
+            mPlayer = null;
+        }
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+    }
+
+
+
+
+
+
+
+    private void startRecording() {
+        mRecorder = new MediaRecorder();
+        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mRecorder.setOutputFile(mFileName);
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            mRecorder.prepare();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+
+        mRecorder.start();
+    }
+
+    private void stopRecording() {
+        mRecorder.stop();
+        mRecorder.release();
+        mRecorder = null;
+    }
+
+    private void startPlaying() {
+        mPlayer = new MediaPlayer();
+        try {
+            mPlayer.setDataSource(mFileName);
+            mPlayer.prepare();
+            mPlayer.start();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+    }
+
+    private void stopPlaying() {
+        mPlayer.release();
+        mPlayer = null;
+    }
+
 }
