f55899808fbac14b09567d845a3cc25f52cbbdf6
tom1209 <tomwhite1209@gmail.com>
2015-05-23 16:00:51 -0400

    Added Login model to store login info
    Model to store login info

--- app/src/main/java/com/petrodevelopment/copdapp/model/Questions.java
+++ app/src/main/java/com/petrodevelopment/copdapp/model/Questions.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * TODO Fix this model to reflect properly our medical data
+ * TODO Fix this model to differentiate questions per providers
  * Created on 22/05/2015.
  */
 public class Questions extends ArrayList {
