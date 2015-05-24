42515c9d9eb1ca86c670638f776ea52a0d77afc5
tom1209 <tomwhite1209@gmail.com>
2015-05-24 10:24:50 -0400

    Underline to New Account on register screen
    Underline to new account on register screen

--- app/src/main/java/com/petrodevelopment/copdapp/activities/HomeRegisterActivity.java
+++ app/src/main/java/com/petrodevelopment/copdapp/activities/HomeRegisterActivity.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,6 +13,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.petrodevelopment.copdapp.R;
@@ -27,6 +30,12 @@ public class HomeRegisterActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_register);
+
+        //Underline New Account
+        TextView textView = (TextView) findViewById(R.id.newAccount);
+        SpannableString content = new SpannableString("New Account");
+        content.setSpan(new UnderlineSpan(), 0, content.length(), 0);
+        textView.setText(content);
 
         //For Sign up button
         Button signup = (Button)findViewById(R.id.signup);
