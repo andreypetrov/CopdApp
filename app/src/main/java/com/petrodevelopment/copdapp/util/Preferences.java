77036a67aacd7090bbebba476c3126bfab265f08
Andrey Petrov <petrov.development@gmail.com>
2015-05-28 15:56:46 -0400

    added preferences

---
+++ app/src/main/java/com/petrodevelopment/copdapp/util/Preferences.java
@@ -0,0 +1,54 @@
+package com.petrodevelopment.copdapp.util;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Encapsulates the access to the shared preferences file for the application
+ * Created by Andrey Petrov on 2014-12-14.
+ */
+public class Preferences {
+    public static final String PREFERENCES_FILE_NAME = "preferences";
+
+    public static final String IS_USER_LOGGED_IN = "is_user_logged_in";
+
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mSharedPreferencesEditor;
+    private Context mContext;
+
+    public Preferences(Context context) {
+        mContext = context;
+    }
+
+    /**
+     * Initialize the preferences. It can be called only after activity's onCreate method
+     */
+    public void init() {
+        mSharedPreferences = mContext.getSharedPreferences(PREFERENCES_FILE_NAME, Context.MODE_PRIVATE);
+        mSharedPreferencesEditor = mSharedPreferences.edit();
+    }
+
+
+    /**
+     * Return the word count saved in the preferences file or if there is none, then return the default one
+     *
+     * @return
+     */
+    public boolean isUserLoggedIn() {
+        return mSharedPreferences.getBoolean(IS_USER_LOGGED_IN, false);
+    }
+
+    private void setIsUserLoggedIn (boolean isUserLoggedIn) {
+        mSharedPreferencesEditor.putBoolean(IS_USER_LOGGED_IN, isUserLoggedIn);
+        mSharedPreferencesEditor.apply();
+    }
+
+    public void logIn() {
+        setIsUserLoggedIn(true);
+    }
+
+    public void logOut() {
+        setIsUserLoggedIn(false);
+    }
+
+}
