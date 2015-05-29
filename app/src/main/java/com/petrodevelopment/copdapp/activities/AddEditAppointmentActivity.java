9a83778f320e957de74fe071729bc2b79b1a6d7d
tom1209 <tomwhite1209@gmail.com>
2015-05-28 23:33:34 -0400

    Changed the default 'Select Providers'
    -Changed default select provider’s to something a little simpler.
    -Also, the first way of doing it made some problems for the information
    taken from the spinner
    -Added a couple different things to the maps (Some that work, some that
    don’t :) )
    -Am trying to centre the maps in a way so the infoWindow isn’t centre
    screen because the message gets cut off

--- app/src/main/java/com/petrodevelopment/copdapp/activities/AddEditAppointmentActivity.java
+++ app/src/main/java/com/petrodevelopment/copdapp/activities/AddEditAppointmentActivity.java
@@ -3,6 +3,7 @@ package com.petrodevelopment.copdapp.activities;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
@@ -13,21 +14,22 @@ import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.DatePicker;
-import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
+
 
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.Projection;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.petrodevelopment.copdapp.adapters.NothingSelectedSpinnerAdapter;
 import com.petrodevelopment.copdapp.R;
 import com.petrodevelopment.copdapp.adapters.EditAddAppointmentProviderListAdapter;
 import com.petrodevelopment.copdapp.model.ClinicianType;
@@ -44,7 +46,7 @@ import java.util.List;
 public class AddEditAppointmentActivity extends ActionBarActivity implements OnClickListener, OnMapReadyCallback {
 
     //Variables for date and time
-    TextView selectTime,selectDate;
+    TextView selectTime,selectDate,selectQuestions;
     private int hour, minute, day, month, year;
 
     //Variables for google maps
@@ -61,6 +63,10 @@ public class AddEditAppointmentActivity extends ActionBarActivity implements OnC
     private Button recordButton;
     private Button saveAppointment;
 
+    //For Testing Questions
+    private String[] questionList;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -71,12 +77,21 @@ public class AddEditAppointmentActivity extends ActionBarActivity implements OnC
         mapFragment.getMapAsync(this);
         initProviders();
         initSpinner();
-        selectDateOrTime();
         initToolbar();
+        setUp();
 
-        setButtonListeners();
+        questionList = new String[]{"...Select Provider to see Quesitons, etc, etc.", "One", "Two", "Three", "Four"};
     }
 
+
+    /*
+     * All listeners methods here
+     */
+    public void setUp()
+    {
+        selectPickers();
+        setButtonListeners();
+    }
 
     public void setButtonListeners()
     {
@@ -134,36 +149,18 @@ public class AddEditAppointmentActivity extends ActionBarActivity implements OnC
     }
 
 
-    /*
-     * Selecting date or time
-     */
-    private void selectDateOrTime()
-    {
-        selectTime = (TextView) findViewById(R.id.select_time);
-        selectTime.setOnClickListener(this);
-
-        selectDate = (TextView) findViewById(R.id.select_date);
-        selectDate.setOnClickListener(this);
-    }
-
 
     private void initSpinner() {
         //Populate Provider Spinner
         final EditAddAppointmentProviderListAdapter providerAdapter = new EditAddAppointmentProviderListAdapter(providers, this, R.layout.cell_provider_add_appointment_list);
         final Spinner providerSpinner = (Spinner) findViewById(R.id.select_provider);
-        //providerSpinner.setAdapter(providerAdapter);
-        providerSpinner.setAdapter(
-                new NothingSelectedSpinnerAdapter(
-                        providerAdapter,
-                        R.layout.cell_spinner_nothing_selected,
-                        this));
-
+        providerSpinner.setAdapter(providerAdapter);
 
         providerSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //populateQuestions();
                 provider = providers.get(position);
-                clinicianType =  ct.get(position);
                 address = provider.address.street;
                 name = "Dr. " + provider.firstName + " " + provider.lastName;
 
@@ -171,8 +168,9 @@ public class AddEditAppointmentActivity extends ActionBarActivity implements OnC
                 lat = Double.parseDouble(provider.address.latitude);
                 lng = Double.parseDouble(provider.address.longitude);
 
-                //Calling method to populate questions
-                // populateQuestions();
+                if (providerSpinner.getSelectedItemPosition() != 0) {
+                    createMarker();
+                }
             }
 
             @Override
@@ -208,7 +206,8 @@ public class AddEditAppointmentActivity extends ActionBarActivity implements OnC
     //For the two pickers, time and date
     @Override
     public void onClick(View view) {
-        if (view== selectDate) {
+
+        if (view == selectDate) {
             //Current D/M/Y
             final Calendar c = Calendar.getInstance();
             day = c.get(Calendar.DAY_OF_MONTH);
@@ -266,36 +265,81 @@ public class AddEditAppointmentActivity extends ActionBarActivity implements OnC
     }
 
 
+    /*
+     * Selecting date or time, or number picker for questions
+     */
+    private void selectPickers()
+    {
+        selectTime = (TextView) findViewById(R.id.select_time);
+        selectTime.setOnClickListener(this);
+
+        selectDate = (TextView) findViewById(R.id.select_date);
+        selectDate.setOnClickListener(this);
+    }
+
+
     //Added for Google Maps 19-05-2015 by Tom
     @Override
     public void onMapReady(GoogleMap map) {
         this.map = map;
 
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lat, lng), 11));
+    }
+
+
+    public void createMarker()
+    {
         BitmapDescriptor bitmapAZURE = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE); //for map marker colour
 
-        //Adding the marker information
-        map.addMarker(new MarkerOptions()
+        Marker info = map.addMarker(new MarkerOptions()
                 .position(new LatLng(lat, lng))
                 .icon(bitmapAZURE)
                 .title(name)
                 .snippet(address));
 
+        setMapScreen(info);
+        info.showInfoWindow();
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lat, lng), 11));
     }
+
+
+    public void setMapScreen(Marker marker)
+    {
+        Projection projection = map.getProjection();
+        LatLng markerLocation = marker.getPosition();
+        Point screenPosition = projection.toScreenLocation(markerLocation);
+
+        Point mappoint = map.getProjection().toScreenLocation(new LatLng(lat, lng));
+        mappoint.set(mappoint.x, mappoint.y+150);
+        map.animateCamera(CameraUpdateFactory.newLatLng(map.getProjection().fromScreenLocation(mappoint)));
+    }
+    /*
+
+    Projection projection = map.getProjection();
+    LatLng markerLocation = marker.getPosition();
+     Point screenPosition = projection.toScreenLocation(markerLocation);
+
+    Point mappoint = googleMap.getProjection().toScreenLocation(new LatLng(latitude, longitude));
+        mappoint.set(mappoint.x, mappoint.y-30);
+        googleMap.animateCamera(CameraUpdateFactory.newLatLng(googleMap.getProjection().fromScreenLocation(mappoint)));
+
+     */
 
 
     //Added by Tom 22-05-2015, for Question once Provider has been selected
     public void populateQuestions()
     {
 
+
         //First hide the textview questions_per_specialist"
-        TextView tv = (TextView) findViewById(R.id.questions_per_specialist);
-        tv.setVisibility(View.GONE);
+        //TextView tv = (TextView) findViewById(R.id.questions_per_specialist);
+       // tv.setVisibility(View.GONE);
 
         //Display Question//
         //Question
-        List<Question> questions = QuestionList.fromAsset(this).questions;
 
+        //List<Question> questions = QuestionList.fromAsset(this).questions;
+        /*
         LinearLayout layout = new LinearLayout(this);
         setContentView(layout);
         layout.setOrientation(LinearLayout.VERTICAL);
@@ -305,7 +349,8 @@ public class AddEditAppointmentActivity extends ActionBarActivity implements OnC
             TextView textView=new TextView(getApplicationContext());
             tv.setText(questions.get(i).toString());
             layout.addView(tv);
-        }
+        }*/
+
     }
 }
 
